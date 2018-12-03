f15e3452b7d8a77dfafee0827c236cf1a3c2d997
RayBowman2017b <RayBowman2017b@gmail.com>
2018-12-03 11:59:00 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+"# WPF_Binding_to_an_Object" 
+"# WPF_Binding_to_an_Object" 
---
+++ z32_git.bat
@@ -0,0 +1,11 @@
+@rem z32_git.bat
+
+echo "# WPF_Binding_to_an_Object" >> README.md
+git init
+@rem  git add README.md
+@rem 
+git add .
+git commit -m "first commit"
+git remote add origin git@github.com:RayBowman2017b/WPF_Binding_to_an_Object.git
+git push -u origin master
+pause
